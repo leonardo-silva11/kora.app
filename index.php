ee2e68e063916e5f45267dc71476dc71b3fa17fa
Leonardo Souza <lss.leonardo.dev@gmail.com>
2024-05-03 20:28:23 -0300

    Atualização e mudanças no index app

--- index.php
+++ index.php
@@ -1,86 +1,43 @@
 <?php
+
+use kora\bin\DefaultResponseKora;
 use kora\bin\RouterKora;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use kora\lib\storage\DirectoryManager;
 
 require_once(__DIR__ . "/vendor/autoload.php");
 
-class Main
+$SoundSingularity = function
+(
+    $useRoutesInProject = false,
+    $useSettingsInProject = false,
+    $displayErrors = true
+)
 {
-    private string $nameProject;
-    private static $instance = null;
-    private static $exceptionSettings = [
-        'line' => true,
-        'file' => true,
-        'details' => true,
-        'defaultHiddenMessage' => 'hidden due to system settings'
-    ];
-
-    public function __construct()
+    try 
     {
-        ini_set('display_errors', 1);
-        $this->nameProject = basename(__DIR__);
-    }
+        $pathOfProject = __DIR__;
+        $nameOfProject = basename(__DIR__);
+        
+        ini_set('display_errors',$displayErrors);
+    
+        $defaultStorage = new DirectoryManager($nameOfProject);
 
-    private function lastInterruptionException(Throwable $th)
+        RouterKora::start
+        ([
+            'pathOfProject' => $pathOfProject,
+            'nameOfProject' => $nameOfProject,
+            'useRoutesInProject' => $useRoutesInProject,
+            'useSettingsInProject' => $useSettingsInProject
+        ],$defaultStorage);
+
+    } 
+    catch (\Throwable $th) 
     {
-        $code = $th->getCode();
-
-        $httpCode = is_int($code) && $code >= 100 && $code <= 599 ? $code : 500;
-
-        $details = method_exists($th,'getDetails') ? $th->getDetails() : [];
-
-        $file = mb_substr(strrchr($th->getFile(), DIRECTORY_SEPARATOR), 1);
-
-        $httpResponse = [
-            'message' => $th->getMessage(),
-            'file' => self::$exceptionSettings['file'] ? $file : self::$exceptionSettings['defaultHiddenMessage'],
-            'line' => self::$exceptionSettings['line'] ? $th->getLine() : self::$exceptionSettings['defaultHiddenMessage'],
-            'details' => self::$exceptionSettings['details'] ? $details : self::$exceptionSettings['defaultHiddenMessage'],
-        ];
-
-        $response = new JsonResponse($httpResponse,intval($httpCode));
-
-        $response->send();
-        exit();
+        (new DefaultResponseKora())
+            ->parseThrowable($th,[
+                'Content-Type' => 'application/json'
+            ]);
     }
+};
 
-    public function getProject()
-    {
-        return $this->nameProject;
-    }
-
-    public function getDefaultStorage() : DirectoryManager
-    {
-        try 
-        {
-            $dir = new DirectoryManager($this->nameProject,[],true);
-
-            return $dir;
-        } 
-        catch (\Throwable $th) 
-        {
-            self::$instance->lastInterruptionException($th);
-        }
-    }
-
-    public static function start()
-    {
-        try 
-        {
-            
-            if(self::$instance == null)
-            {
-                self::$instance = new Main();
-            }
-            
-            RouterKora::start(self::$instance);
-
-        } 
-        catch (\Throwable $th) 
-        {
-            self::$instance->lastInterruptionException($th);
-        }
-    }
-}
-Main::start();
+$SoundSingularity();
